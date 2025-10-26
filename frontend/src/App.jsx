ef9717cf6ff2fe874d08320b223641699bd168e3
Whackalenso <63932931+Whackalenso@users.noreply.github.com>
2025-10-25 22:51:36 -0700

    landing init

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import SessionPage from "./pages/SessionPage";
+import LandingPage from "./pages/LandingPage";
 import ResultsPage from "./pages/ResultsPage";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/create" element={<HomePage />} />
       <Route path="/session/:id" element={<SessionPage />} />
       <Route path="/results/:id" element={<ResultsPage />} />
     </Routes>
